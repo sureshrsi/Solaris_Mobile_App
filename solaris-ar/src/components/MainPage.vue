4f868a1abc25e26c08aa40c0ed58d0f81e45ae3b
vamsikiran15 <vamsikiran15@gmail.com>
2024-05-28 13:21:39 +0530

    Added cards to the second page

--- solaris-ar/src/components/MainPage.vue
+++ solaris-ar/src/components/MainPage.vue
@@ -4,7 +4,7 @@
       <ion-img
         class="imgsize"
         src="../src/assets/img/SOLARISLogo.png"
-        alt="The Wisconsin State Capitol building in Madison, WI at night"
+        alt="Solaris"
       ></ion-img
     ></ion-toolbar>
     <ion-content>
@@ -76,26 +76,173 @@
         </ion-row>
       </ion-card>
       <ion-card>
-        <ion-card>
           <ion-row>
             <ion-col>
-              <ion-img src="../src/assets/img/NSGRAD.jpg" />
+                <div class="image-container">
+                  <ion-img src="../src/assets/img/NSGRAD.jpg" />
+                  <div class="overlay-text">Namsai</div>
+                </div>
             </ion-col>
             <ion-col>
-              <ion-img src="../src/assets/img/LSGRAD.jpg" />
+                <div class="image-container">
+                  <ion-img src="../src/assets/img/LSGRAD.jpg" />
+                  <div class="overlay-text">Lower Subansari</div>
+                </div>
             </ion-col>
             <ion-col>
-              <ion-img src="../src/assets/img/TWGRAD.jpg" />
+                <div class="image-container">
+                  <ion-img src="../src/assets/img/TWGRAD.jpg" />
+                  <div class="overlay-text">Tawang</div>
+                </div>
             </ion-col>
           </ion-row>
-        </ion-card>
-        <ion-card>
-          <ion-row>
-            <ion-col> adsfkj </ion-col>
-            <ion-col> adfksjdf </ion-col>
-          </ion-row>
-        </ion-card>
       </ion-card>
+      <ion-grid>
+      <ion-row>
+        <ion-col size="3">
+      <ion-card class="smallCardSize">
+        <div class="ion-text-center">
+        <ion-img src="../src/assets/img/Map_Console_Logo.png"></ion-img>
+        <ion-text>Namsai</ion-text>
+      </div>
+      </ion-card>
+    </ion-col>
+    <ion-col size="9">
+      <ion-card class="dataCardSize">
+        <ion-row>
+          <ion-col>
+            <ion-text>Tawang</ion-text>
+          </ion-col>
+          <ion-col class="ion-text-end">
+            <ion-text>Head Quarters</ion-text>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col>
+            <ion-text>District</ion-text>
+          </ion-col>
+          <ion-col class="ion-text-end">
+            <ion-text>Tawang</ion-text>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col>
+            <ion-text>Population : 49977</ion-text>
+          </ion-col>
+        </ion-row>
+        <ion-row class="custom-line"></ion-row>
+        <ion-row>
+          <ion-col>
+          <ion-text>Area : 2571.89 Sq Km</ion-text>
+        </ion-col>
+        <ion-col class="ion-text-end">
+          <ion-text>Circle 11</ion-text>
+        </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col>
+            <ion-text>
+              Revenue Villages : 218
+            </ion-text>
+          </ion-col>
+          <ion-col class="ion-text-end">
+            <ion-text>Block 6</ion-text>
+          </ion-col>
+        </ion-row>
+      </ion-card> 
+    </ion-col>
+    </ion-row>
+  </ion-grid>
+  <ion-grid>
+    <ion-row>
+      <ion-col>
+        <ion-card class="cardSize">
+          <ion-img class="imgSize" src="../src/assets/img/BaseMap.png"></ion-img>
+          <ion-row class="custom-line"></ion-row>
+         <ion-card-subtitle class="ion-text-center ion-padding">Base Map</ion-card-subtitle>
+        </ion-card>
+      </ion-col>
+      <ion-col>
+        <ion-card class="cardSize">
+          <ion-img class="imgSize" src="../src/assets/img/LULC.png"></ion-img>
+          <ion-row class="custom-line"></ion-row>
+          <ion-card-subtitle class="ion-text-center ion-padding">Land Use Land Cover</ion-card-subtitle>
+        </ion-card>
+      </ion-col>
+    </ion-row>
+  </ion-grid>
+  <ion-grid>
+    <ion-row>
+      <ion-col>
+        <ion-card class="cardSize">
+        <ion-img class="imgSize" src="../src/assets/img/SlopeInformation.png"></ion-img>
+        <ion-row class="custom-line"></ion-row>
+        <ion-card-subtitle class="ion-text-center ion-padding">Slope Information</ion-card-subtitle>
+        </ion-card>
+      </ion-col>
+      <ion-col>
+        <ion-card class="cardSize">
+          <ion-img class="imgSize" src="../src/assets/img/Hydrogeomorphology.png"></ion-img>
+          <ion-row class="custom-line"></ion-row>
+          <ion-card-subtitle class="ion-text-center ion-padding">Hydrogeo Morphology</ion-card-subtitle>
+        </ion-card>
+      </ion-col>
+    </ion-row>
+  </ion-grid>
+  <ion-grid>
+    <ion-row>
+      <ion-col>
+        <ion-card class="cardSize">
+        <ion-img class="imgSize" src="../src/assets/img/Soil.png"></ion-img>
+        <ion-row class="custom-line"></ion-row>
+        <ion-card-subtitle class="ion-text-center ion-padding">Soil Information</ion-card-subtitle>
+        </ion-card>
+      </ion-col>
+      <ion-col>
+        <ion-card class="cardSize">
+        <ion-img class="imgSize" src="../src/assets/img/LandDegredation.png"></ion-img>
+        <ion-row class="custom-line"></ion-row>
+        <ion-card-subtitle class="ion-text-center ion-padding">Land Degradation</ion-card-subtitle>
+        </ion-card>
+      </ion-col>
+    </ion-row>
+  </ion-grid>
+  <ion-grid>
+    <ion-row>
+      <ion-col>
+        <ion-card class="cardSize">
+         <ion-img class="imgSize" src="../src/assets/img/LandCapability.png"></ion-img>
+         <ion-row class="custom-line"></ion-row>
+         <ion-card-subtitle class="ion-text-center ion-padding">Land Capability</ion-card-subtitle>
+        </ion-card>
+      </ion-col>
+      <ion-col>
+        <ion-card class="cardSize">
+         <ion-img class="imgSize" src="../src/assets/img/LandIrrigability.png"></ion-img>
+         <ion-row class="custom-line"></ion-row>
+         <ion-card-subtitle class="ion-text-center ion-padding">Land Irrigability</ion-card-subtitle>
+        </ion-card>
+      </ion-col>
+    </ion-row>
+  </ion-grid>
+  <ion-grid>
+    <ion-row>
+      <ion-col>
+        <ion-card class="cardSize">
+       <ion-img class="imgSize" src="../src/assets/img/ActionPlan.png"></ion-img>
+       <ion-row class="custom-line"></ion-row>
+       <ion-card-subtitle class="ion-text-center ion-padding">Action Plan</ion-card-subtitle>
+        </ion-card>
+      </ion-col>
+      <ion-col>
+        <ion-card class="cardSize">
+          <ion-img class="imgSize" src="../src/assets/img/CropSuitability.png"></ion-img>
+          <ion-row class="custom-line"></ion-row>
+          <ion-card-subtitle class="ion-text-center ion-padding">Crop Suitability</ion-card-subtitle>
+        </ion-card>
+      </ion-col>
+    </ion-row>
+  </ion-grid>
     </ion-content>
   </ion-page>
 </template>
@@ -139,5 +286,62 @@ export default {
 }
 object path:hover {
   fill: red;
+}
+.cardLength{
+  width: 26vh;
+  height:13vh
+}
+.smallCardLength{
+  width: 10vh;
+  height:6vh
+}
+.image-container {
+  position: relative;
+  display: inline-block;
+  border-radius: 1vh;
+  overflow: hidden;
+}
+
+.image-container img {
+  width: 100%;
+  height: auto;
+}
+
+.overlay-text {
+  position: absolute;
+  top: 85%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  /* background-color: rgba(
+    0,
+    0,
+    0,
+    0.5
+  ); */
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.custom-line {
+  border-width: thin;
+  border: inset;
+}
+
+.cardSize{
+  height: 19vh;
+  width:17vh
+}
+.imgSize{
+  height: 13vh;
+  width:17vh
+}
+.dataCardSize{
+  height: 13vh;
+  width:30vh
+}
+.smallCardSize{
+  height: 13vh;
+  width:10vh
 }
 </style>
