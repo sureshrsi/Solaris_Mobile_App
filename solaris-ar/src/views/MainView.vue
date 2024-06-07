038ca5912c5a4cf2d75d6b24889bb0c36e7dead2
unknown <suresh.rsisoft@gmail.com>
2024-06-07 17:36:55 +0530

    added exit function in home page

--- solaris-ar/src/views/MainView.vue
+++ solaris-ar/src/views/MainView.vue
@@ -3,7 +3,37 @@
 </template>
 <script>
 import MainPage from "../components/MainPage.vue";
+import { defineComponent, onMounted, onUnmounted } from "vue";
+import { App } from "@capacitor/app";
+import { useRouter } from "vue-router";
 export default {
+  setup() {
+    const router = useRouter();
+
+    const handleBackButton = () => {
+      if (router.currentRoute.value.path === "/main") {
+        App.exitApp();
+      } else {
+        router.back();
+      }
+    };
+
+    onMounted(() => {
+      App.addListener("backButton", handleBackButton);
+    });
+
+    onUnmounted(() => {
+      App.removeListener("backButton", handleBackButton);
+    });
+
+    // Listen for Ionic's ionBackButton event to handle swipe back gesture
+    document.addEventListener("ionBackButton", (ev) => {
+      ev.detail.register(10, (processNextHandler) => {
+        handleBackButton();
+        processNextHandler();
+      });
+    });
+  },
   components: {
     MainPage,
   },
